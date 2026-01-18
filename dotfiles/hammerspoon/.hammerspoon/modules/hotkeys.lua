a2d1781a15949d08eb85f56f63a8b8ac45bcde4f
Artem Lazarev <artem.lazarev2006@gmail.com>
2026-01-18 18:31:22 +0500

    replace vscode by zed

--- dotfiles/hammerspoon/.hammerspoon/modules/hotkeys.lua
+++ dotfiles/hammerspoon/.hammerspoon/modules/hotkeys.lua
@@ -3,71 +3,35 @@ local vpn = require("modules.vpn")
 local hotkeys = {}
 
 function hotkeys.setup()
-    -- ITerm
-    hs.hotkey.bind({"cmd", "ctrl"}, hs.keycodes.map["t"], function()
-        hs.application.launchOrFocus("ITerm")
-    end)
+    local app_map = {
+        ["t"] = "ITerm",
+        ["b"] = "Zen",
+        ["g"] = "Perplexity",
+        ["c"] = "Zed",
+        ["o"] = "Obsidian",
+        ["f"] = "Finder",
+        ["h"] = "NeoHtop",
+        ["s"] = "Safari",
+        ["i"] = "iPhone Mirroring",
+        ["e"] = "Calendar",
+        ["r"] = "Reminders"
+    }
 
-    -- Zen
-    hs.hotkey.bind({"cmd", "ctrl"}, hs.keycodes.map["b"], function()
-        hs.application.launchOrFocus("Zen")
-    end)
-
-    -- Perplexity
-    hs.hotkey.bind({"cmd", "ctrl"}, hs.keycodes.map["g"], function()
-        hs.application.launchOrFocus("Perplexity")
-    end)
-
-    -- Visual Studio Code
-    hs.hotkey.bind({"cmd", "ctrl"}, hs.keycodes.map["c"], function()
-        hs.application.launchOrFocus("Visual Studio Code")
-    end)
-
-    -- Obsidian
-    hs.hotkey.bind({"cmd", "ctrl"}, hs.keycodes.map["o"], function()
-        hs.application.launchOrFocus("Obsidian")
-    end)
-    
-    -- Finder
-    hs.hotkey.bind({"cmd", "ctrl"}, hs.keycodes.map["f"], function()
-        hs.application.launchOrFocus("Finder")
-    end)
-
-    -- NeoHtop
-    hs.hotkey.bind({"cmd", "ctrl"}, hs.keycodes.map["h"], function()
-        hs.application.launchOrFocus("NeoHtop")
-    end)
-    
-    -- Safari
-    hs.hotkey.bind({"cmd", "ctrl"}, hs.keycodes.map["s"], function()
-      hs.application.launchOrFocus("Safari")
-    end)
-
-    -- iPhone Mirroring
-    hs.hotkey.bind({"cmd", "ctrl"}, hs.keycodes.map["i"], function()
-        hs.application.launchOrFocus("iPhone Mirroring")
-    end)
-    
-    -- Calendar
-    hs.hotkey.bind({"cmd", "ctrl"}, hs.keycodes.map["e"], function()
-        hs.application.launchOrFocus("Calendar")
-    end)
-    
-    -- Reminders
-    hs.hotkey.bind({"cmd", "ctrl"}, hs.keycodes.map["r"], function()
-        hs.application.launchOrFocus("Reminders")
-    end)
+    for key, app_name in pairs(app_map) do
+        hs.hotkey.bind({ "cmd", "ctrl" }, hs.keycodes.map[key], function()
+            hs.application.launchOrFocus(app_name)
+        end)
+    end
 
     -- VPN
-    hs.hotkey.bind({"cmd", "ctrl"}, hs.keycodes.map["p"], function()
+    hs.hotkey.bind({ "cmd", "ctrl" }, hs.keycodes.map["p"], function()
         vpn.toggle()
     end)
 
     -- Sleep
-    hs.hotkey.bind({"cmd", "ctrl"}, hs.keycodes.map["m"], function()
+    hs.hotkey.bind({ "cmd", "ctrl" }, hs.keycodes.map["m"], function()
         hs.caffeinate.systemSleep()
     end)
 end
 
 return hotkeys
-
